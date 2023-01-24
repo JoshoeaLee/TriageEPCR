2098dfa13f74988da86978c9afe315f34368fd1a
Meiqiao <chenmeiqiao940926@gmail.com>
2023-01-25 11:49:36 +1300

    add yo yo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Cool guy</Text>
+      <Text>Cool guy yoyo</Text>
       <StatusBar style="auto" />
     </View>
   );
